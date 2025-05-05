6d817d9e809cb42a2b13e565e1936ca829fd0fc8
Jesse Bremer <jessesrbremer@gmail.com>
2025-05-05 12:34:02 -0400

    Playing with inputs

--- task/task.py
+++ task/task.py
@@ -1,14 +1,4 @@
-# Write your code below this line 👇
+userName = input("What is your name?\n")
+print("My name is" + " " + userName + "!!")
 
-#New Lines
-print("Hello World!\n\nHello world!\n\nHello world!\n")
-
-#Concatenation
-print("Hello" + " Jesse!\n")
-
-#Convert these to strings:
-print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
-print("2. Knead the dough for 10 minutes.")
-print("3. Add 3g of Salt.")
-print("4. Leave to rise for 2 hours.")
-print("5. Bake at 200 degrees C for 30 minutes.")
+print("Hello " + input("What is your name?") + "!")
